8d7462a7a4a48cbf95e99a4d2a4e05697e5d55ca
N-feng <308561157@qq.com>
2024-06-07 16:15:53 +0800

    init🎉: first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://rested-turkey-35.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
